7e47b1c79ba4819e6b1ee4e30876a601f9691b26
Charles W. Drummond Jr <cdrum360@live.com>
2017-05-13 13:59:46 -0400

    Add project files.

---
+++ Reflection/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Reflection
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class MyAttributeAttribute : Attribute
+    {
+        public string Description { get; set; }
+        public string VersionNumber { get; set; }
+        public string ReviewerId { get; set; }
+        
+        public MyAttributeAttribute(string desc, string ver)
+        {
+            Description = desc;
+            VersionNumber = ver;
+        }
+    }
+        
+    [MyAttribute("Check it out", "3.1")]
+    class MyClass { }   
+  
+    public class Program
+    {
+        static void Main()
+        {
+            Type t = typeof(MyClass);
+            object[] attArr = t.GetCustomAttributes(false);
+            foreach (Attribute a in attArr)
+            {
+                MyAttributeAttribute attr = a as MyAttributeAttribute;
+                if (null != attr)
+                {
+                    Console.WriteLine("Description : {0}", attr.Description);
+                    Console.WriteLine("Version Number : {0}", attr.VersionNumber);
+                    Console.WriteLine("Reviewer ID : {0}", attr.ReviewerId);
+                }
+            }
+
+            Console.ReadLine();
+        }
+     }
+
+}
+
